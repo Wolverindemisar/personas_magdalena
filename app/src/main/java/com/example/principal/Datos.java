2c6976c3c52658fd3db3c1053a70b5cfed0732ec
Herwin Rosado <03host03@gmail.com>
2019-07-12 21:10:38 -0500

    Clase Datos realizada

---
+++ app/src/main/java/com/example/principal/Datos.java
@@ -0,0 +1,18 @@
+package com.example.principal;
+
+import java.util.ArrayList;
+
+public class Datos {
+
+    private static ArrayList<Persona> personas = new ArrayList<>();
+
+    public static void guardar(Persona p)
+    {
+        personas.add(p);
+    }
+
+    public static ArrayList<Persona> obtener()
+    {
+        return personas;
+    }
+}
